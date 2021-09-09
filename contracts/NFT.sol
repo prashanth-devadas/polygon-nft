19a3cd85dd2fceaa1c68048e160d873e6a9cd0bd
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-09-09 16:22:27 -0400

    marketplace to sell ma NFTs

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -17,6 +17,12 @@ contract NFT is ERC721URIStorage {
     }
 
     function createToken(string memory tokenURI) public returns (uint256) {
-        
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
     }
 }
