d2b7421720d833e36f9fcfa325046a1d2020550e
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-09-18 23:42:33 -0400

    deploy script changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy("Hello, NFT!");
 
-  await greeter.deployed();
+  await nftMarket.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("NFTMarket deployed to:", nftMarket.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
