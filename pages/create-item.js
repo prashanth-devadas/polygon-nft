0754345ad2cffffc48398aa86a5300d10290990d
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-09-19 20:08:35 -0400

    changes to create item page

--- pages/create-item.js
+++ pages/create-item.js
@@ -13,3 +13,27 @@ import {
   
   import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
   import NFTMarket from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+
+  export default function CreateItem() {
+      const [fileUrl, setFileUrl] = useState(null)
+      const [formInput, updateFormInput] = useState({price: '', name: '', description:''})
+      const router = useRouter()
+      
+      async function onChange(e) {
+          const file = e.target.files[0]
+
+          try {
+            const added = await client.add(
+                file,
+                // this block takes care of file upload
+                {
+                    progress: (prog) => console.log(`received: ${prog}`);
+                }
+            )
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            setFileUrl(url)
+          } catch(e){
+              console.log(e);
+          }
+      }
+  }
